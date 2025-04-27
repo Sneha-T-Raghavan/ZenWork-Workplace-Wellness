cacdef55b5d46c6c65a8a452e64b050023c399d5
ananyag27 <ananyaganapathi.27@gmail.com>
2025-04-27 16:16:37 +0530

    Uploaded bingo game and break timer quest

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,8 @@ import ResetPassword from './pages/ResetPassword'
 import EmailVerify from './pages/EmailVerify'
 import Pixelgame from './pages/Pixelgame'
 import Home from './pages/Home'
+import Bingogame from './pages/Bingogame'
+import BreakTimerQuest from './pages/BreakTimerQuest'
 
 
 const App = () => {
@@ -19,6 +21,8 @@ const App = () => {
         <Route path="/reset-password" element={<ResetPassword />} />  
         <Route path="/email-verify" element={<EmailVerify />} />
         <Route path="/pixel-game" element={<Pixelgame />} />  
+        <Route path="/bingo-game" element={<Bingogame />} />
+        <Route path="/break-timer-quest" element={<BreakTimerQuest />} />
       </Routes>
     </div>
   )
